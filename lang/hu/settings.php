0d5150d318214592f2a1e9eda38a5e388f299187
Szabó Tamás <saintszt@gmail.com>
2011-07-20 14:35:32 +0200

    Hungarian translation added

---
+++ lang/hu/settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['dayheaderfmt'] = 'Dátum formátum a naphoz. (strftime formátum)';
+$lang['lasttime'] = 'Ennyi időnél nem régebbi változtatások jelenjenek meg. 0 = minden látszik (másodperc)';//edited
