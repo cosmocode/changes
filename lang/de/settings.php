2e8133346d663bae96cf7014386b3d91b7465ccd
Sunny <pl787878@gmail.com>
2015-12-05 22:25:41 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,5 +1,9 @@
 <?php
 
-$lang['dayheaderfmt'] = 'Datumsformat der Überschrift bei Verwendung des dayheaders-Parameters. (strftime format)';
-$lang['maxage'] = 'Maximales Alter der Einträge in Sekunden. (0 = deaktiviert)';
-$lang['listmedia'] = 'Änderungen in den Medien-Dateien einbinden?';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['dayheaderfmt']          = 'Datumsformat der Überschrift bei Verwendung des dayheaders-Parameters. (strftime format)';
+$lang['maxage']                = 'Maximales Alter der Einträge in Sekunden. (0 = deaktiviert)';
+$lang['listmedia']             = 'Änderungen in den Medien-Dateien einbinden?';
