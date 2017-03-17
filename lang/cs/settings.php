3ef0d072f6fd676dc2b17d27c70182ab2af8fc99
Quark66 <mkucera66@seznam.cz>
2017-03-17 09:05:10 +0100

    translation update

---
+++ lang/cs/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Quark66 <mkucera66@seznam.cz>
+ */
+$lang['dayheaderfmt']          = 'Formát datumů v záhlaví dnů (formát strftime)';
+$lang['maxage']                = 'Ukázat nejstarší změny. 0 = vypnuto (v sekundách)';
+$lang['listmedia']             = 'Zahrnout změny souborů médií';
